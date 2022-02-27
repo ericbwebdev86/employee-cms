223c523c31aab0ed812064dd7d8ae60669e68aed
unknown <ericbwebdev86@gmail.com>
2022-02-27 11:50:38 -0600

    added comments, removed console.log statements, removed unnecessary code

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 
+//connection params for database
 const db = mysql.createConnection(
     {
         host: 'localhost',
@@ -12,6 +13,8 @@ const db = mysql.createConnection(
     },
     console.log('Connecting to the Employee CMS database!')
 );
+
+//connect to db on app launch, also call appprompt function
 db.connect(function (err) {
     if (!err) {
         console.log('  ---  Connection Success!  ---  ');
@@ -19,12 +22,8 @@ db.connect(function (err) {
     } else {
         throw err;
     }
-
 });
-
-
-
-
+//main inquirer prompt for the app
 const appPrompt = () => {
     console.log(`
     ()()()()()()()()()
@@ -36,10 +35,10 @@ const appPrompt = () => {
             type: 'list',
             messsage: 'What would you like to do?',
             name: 'listChoices',
-            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Quit program']
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
         }
     ]).then((answers) => {
-        console.log(answers.listChoices);
+        //if view all depts is chosen then display the results of query
         if (answers.listChoices === 'View all departments') {
             db.query("SELECT * FROM department;",
                 function (err, results, fields) {
@@ -48,13 +47,15 @@ const appPrompt = () => {
                 })
         }
         if (answers.listChoices === 'View all roles') {
+            //if view roles is chosen then display the results of query
             db.query("SELECT employee_role.title, department.department_name AS department, employee_role.salary FROM employee_role INNER JOIN department ON employee_role.department_id = department.id;",
                 function (err, results, fields) {
                     console.table(results);
                     appPrompt();
                 })
-        } //fn ln title dept salary manager
+        }
         if (answers.listChoices === 'View all employees') {
+            //if view all employees is chosen then display the results of query
             db.query(`SELECT employee.first_name, employee.last_name, employee_role.title, department.department_name AS department, employee_role.salary, CONCAT (manager.first_name, " ", manager.last_name) AS manager FROM employee LEFT JOIN employee_role ON employee.role_id = employee_role.id LEFT JOIN department ON employee_role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id;`,
                 function (err, results, fields) {
                     console.table(results);
@@ -69,7 +70,6 @@ const appPrompt = () => {
         }
         if (answers.listChoices === 'Add an employee') {
             addEmployee();
-
         }
         if (answers.listChoices === 'Update an employee role') {
             updateEmployeeRole();
@@ -83,7 +83,6 @@ addDept = () => {
     ()()()()()()()()()()()
     `)
     inquirer.prompt([
-
         {
             type: 'input',
             name: 'deptInput',
@@ -99,6 +98,7 @@ addDept = () => {
         }
     ]).then(answers => {
         const sql = `INSERT INTO department (department_name) VALUES (?)`;
+        //take department input and use it as the value in the above sql query then return to main menu
         db.query(sql, answers.deptInput, function (err, results, fields) {
             appPrompt();
         })
@@ -112,9 +112,8 @@ addEmployeeRole = () => {
     `);
     const sql = 'select department.id, department.department_name from department';
     db.query(sql, (err, results) => {
+        //run query to get department ids and department names from department, organize in array and use as choices
         const department = results.map(({ id, department_name }) => ({ name: department_name, value: id }));
-        console.log(results);
-
         inquirer.prompt([
             {
                 type: 'input',
@@ -150,12 +149,12 @@ addEmployeeRole = () => {
             }
         ]).then(answers => {
             const sql = 'INSERT INTO employee_role (title, salary, department_id) VALUES (?, ?, ?)'
+            //use the inputs from the 3 questions as values in the above query to make new role, then return to main menu
             db.query(sql, [answers.roleInput, answers.salaryInput, answers.roleDeptInput], function (err, results, fields) {
                 appPrompt();
             })
         });
     })
-
 }
 addEmployee = () => {
     console.log(`
@@ -194,6 +193,7 @@ addEmployee = () => {
         const newEmployee = [answers.firstName, answers.lastName];
         let roleQuery = 'SELECT employee_role.id, employee_role.title from employee_role'
         db.query(roleQuery, (err, results) => {
+            //get id and title and map them to array for use in choices
             const roleArray = results.map(({ id, title }) => ({ name: title, value: id }));
 
             inquirer.prompt([
@@ -208,6 +208,7 @@ addEmployee = () => {
             newEmployee.push(role);
             const mgrQuery = 'SELECT * FROM employee';
             db.query(mgrQuery, function (err, results) {
+                //get all of the employees put the first and last names together mapped in an array for choices
                 mgrArray = results.map(({ id, first_name, last_name }) => ({name: first_name + " " + last_name, value: id }));
                 inquirer.prompt([
                     {
@@ -220,16 +221,16 @@ addEmployee = () => {
             let manager = answers.mgrChoice
             newEmployee.push(manager);
             const sql = 'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES(?, ?, ?, ?)';
+            //take all of the inputs and use them as values for the above query, then go back to main menu
             db.query(sql, newEmployee, function (err, results, fields) {
                 appPrompt();
-            })
-        })
-            })
-        })
-        })
-    })
-        
-}
+            });
+        });
+    });
+});
+});
+});      
+};
 updateEmployeeRole = () => {
     console.log(`
     ()()()()()()()()()()()
@@ -263,17 +264,17 @@ updateEmployeeRole = () => {
                     ]).then(answers => {
                         let selectedRole = answers.role;
                         queryValues.push(selectedRole);
-                        console.log(queryValues);
                         queryValues[0] = selectedRole;
                         queryValues[1] = selectedEmployee;
                         const updateRole = 'UPDATE employee SET role_id = ? WHERE id = ?';
+                        //took the input from both questions that are asked in an order that makes sense to humans
+                        //HOWEVER the query wants the values for role THEN employee
+                        //had to rearragne the values for the indeces int he array then use the array as values for the query, then return to main menu
                         db.query(updateRole, queryValues, (err, results) => {
-                            console.log(results);
-                            console.log('line 269', queryValues);
                             appPrompt();
-                        })
-                    })
-                })
-            })
-    })
-}
+                        });
+                    });
+                });
+            });
+        });
+};
